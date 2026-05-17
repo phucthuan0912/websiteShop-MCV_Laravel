27753b35f000330d56c28b253d0b0e2186277641
Thuan Nguyen Phuc <thanhthaopl2011@gmail.com>
2026-05-18 06:34:42 +0700

    FEAT: Category: add category  model,migration,controller and add category admin

---
+++ app/Http/Controllers/admin/CategoryController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Http\Requests\CategoryRequest;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        $categories = Category::all();
+        return view('admin.category.list', compact('categories'));
+    }
+
+    public function create()
+    {
+        return view('admin.category.create');
+    }
+
+    public function storeCategory(CategoryRequest $request)
+    {
+        $data = $request->validated();
+
+        if (Category::create($data)) {
+            return redirect()->route('admin.category.list')->with('success', 'Thêm danh mục thành công');
+        } else {
+            return redirect()->back()->with('error', 'Thêm danh mục thất bại');
+        }
+    }
+
+    public function deleteCategory($id)
+    {
+        $category = Category::find($id);
+        $category->delete();
+        return redirect()->back()->with('success', 'Xóa danh mục thành công');
+    }
+}
