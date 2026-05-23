bce91f9710f5068850d16d239c9957e3863d9b4b
Nguyen Phuc Thuan <thanhthaopl2011@gmail.com>
2026-05-23 21:08:47 +0700

    feat(cart): add shopping cart page and cart management functionality

---
+++ app/Http/Controllers/frontend/CartController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers\frontend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CartController extends Controller
+{
+    public function index(){
+        
+        $cart = session()->get('cart', []);
+        $cart = collect($cart)->map(function ($item) {
+            return (object) $item;
+        });
+        $tax = 2;
+        $subTotal = 0; 
+        $final_subTotal = 0; 
+        foreach ($cart as $item) {
+            $subTotal+= $item->price * $item->quantity;
+        }
+        if($subTotal > 0 ) {
+            $final_subTotal = $tax+$subTotal;     
+        } else{
+            $subTotal = 2;
+            $final_subTotal = $tax;
+
+        }
+        return view ('frontend.cart.cart', compact('cart', 'final_subTotal','subTotal','tax' ));
+    }
+
+    public function updateCart(Request $request)
+    {
+        $id = $request->id;
+        $type = $request->type;
+        $cart = session()->get('cart', []);
+        
+        if (isset($cart[$id])) {
+            if ($type == 'plus') {
+                $cart[$id]['quantity']++;
+            } elseif ($type == 'minus') {
+                $cart[$id]['quantity']--;
+                if ($cart[$id]['quantity'] <= 0) {
+                    unset($cart[$id]);
+                }
+            }
+            session()->put('cart', $cart);
+        }
+        
+        $data = $this->calculateData($cart, $id);
+        return response()->json($data);
+    }
+    public function removeCart(Request $request)
+    {
+        $id = $request->id;
+        $cart = session()->get('cart', []);
+        
+        if (isset($cart[$id])) {
+            unset($cart[$id]);
+            session()->put('cart', $cart);
+        }
+
+        $data = $this->calculateData($cart);
+        return response()->json($data);
+    }
+    private function calculateData($cart, $id = null)
+    {
+        $subtotal = 0;
+        $totalQuantity = 0;
+        
+        foreach ($cart as $item) {
+            $subtotal += $item['quantity'] * $item['price'];
+            $totalQuantity += $item['quantity'];
+        }
+        $eco_tax = 2;
+        $total = $subtotal > 0 ? $subtotal + $eco_tax : 0;
+        $eco_tax = $subtotal > 0 ? $eco_tax : 0; 
+
+        return [
+            'status' => true,
+            'qty' => ($id && isset($cart[$id])) ? $cart[$id]['quantity'] : 0,
+            'lineTotal' => ($id && isset($cart[$id])) ? number_format($cart[$id]['quantity'] * $cart[$id]['price']) : 0,
+            'subTotal' => number_format($subtotal),
+            'tax' => number_format($eco_tax),
+            'total' => number_format($total),
+            'total_quantity' => $totalQuantity
+        ];
+    }
+}
---
+++ resources/views/frontend/cart/cart.blade.php
@@ -0,0 +1,207 @@
+@extends('frontend.layouts.app')
+@section('content')
+<section id="cart_items">
+		<div class="container">
+			<div class="breadcrumbs">
+				<ol class="breadcrumb">
+				  <li><a href="#">Home</a></li>
+				  <li class="active">Shopping Cart</li>
+				</ol>
+			</div>
+			<div class="table-responsive cart_info">
+				<table class="table table-condensed">
+					<thead>
+						<tr class="cart_menu">
+							<td class="image">Item</td>
+							<td class="description"></td>
+							<td class="price">Price</td>
+							<td class="quantity">Quantity</td>
+							<td class="total">Total</td>
+							<td></td>
+						</tr>
+					</thead>
+					<tbody>
+                        @forelse($cart as $id => $item)
+						<tr class="row-item-{{ $id }}">
+							<td class="cart_product">
+								<a href=""><img src="{{ $item->image }}" alt="" style="width: 100px; height: 140px;"></a>
+							</td>
+							<td class="cart_description">
+								<h4><a href="">{{ $item->name }}</a></h4>
+								<p>Web ID: {{ $id }}</p>
+							</td>
+							<td class="cart_price">
+								<p>${{ number_format($item->price) }}</p>
+							</td>
+							<td class="cart_quantity">
+								<div class="cart_quantity_button">
+									<a class="cart_quantity_up btn-update-qty" href="javascript:void(0)" data-id="{{ $id }}" data-type="plus"> + </a>
+									<input class="cart_quantity_input qty-input-{{ $id }}" type="text" name="quantity" value="{{ $item->quantity }}" autocomplete="off" size="2" readonly>
+									<a class="cart_quantity_down btn-update-qty" href="javascript:void(0)" data-id="{{ $id }}" data-type="minus"> - </a>
+								</div>
+							</td>
+							<td class="cart_total">
+								<p class="cart_total_price item-total-{{ $id }}">${{ number_format($item->price * $item->quantity) }}</p>
+							</td>
+							<td class="cart_delete">
+								<a class="cart_quantity_delete btn-remove-item" href="javascript:void(0)" data-id="{{ $id }}"><i class="fa fa-times"></i></a>
+							</td>
+						</tr>
+                    @empty
+					<tr>
+						<td colspan="6">
+							<div class="alert alert-info text-center">
+								<p>Giỏ hàng trống!</p>
+							</div>
+						</td>
+					</tr>
+					@endforelse
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</section> <!--/#cart_items-->
+
+	<section id="do_action">
+		<div class="container">
+			<div class="heading">
+				<h3>What would you like to do next?</h3>
+				<p>Choose if you have a discount code or reward points you want to use or would like to estimate your delivery cost.</p>
+			</div>
+			<div class="row">
+				<div class="col-sm-6">
+					<div class="chose_area">
+						<ul class="user_option">
+							<li>
+								<input type="checkbox">
+								<label>Use Coupon Code</label>
+							</li>
+							<li>
+								<input type="checkbox">
+								<label>Use Gift Voucher</label>
+							</li>
+							<li>
+								<input type="checkbox">
+								<label>Estimate Shipping & Taxes</label>
+							</li>
+						</ul>
+						<ul class="user_info">
+							<li class="single_field">
+								<label>Country:</label>
+								<select>
+									<option>United States</option>
+									<option>Bangladesh</option>
+									<option>UK</option>
+									<option>India</option>
+									<option>Pakistan</option>
+									<option>Ucrane</option>
+									<option>Canada</option>
+									<option>Dubai</option>
+								</select>
+								
+							</li>
+							<li class="single_field">
+								<label>Region / State:</label>
+								<select>
+									<option>Select</option>
+									<option>Dhaka</option>
+									<option>London</option>
+									<option>Dillih</option>
+									<option>Lahore</option>
+									<option>Alaska</option>
+									<option>Canada</option>
+									<option>Dubai</option>
+								</select>
+							
+							</li>
+							<li class="single_field zip-field">
+								<label>Zip Code:</label>
+								<input type="text">
+							</li>
+						</ul>
+						<a class="btn btn-default update" href="">Get Quotes</a>
+						<a class="btn btn-default check_out" href="">Continue</a>
+					</div>
+				</div>
+				<div class="col-sm-6">
+					<div class="total_area">
+						<ul>
+							<li>Cart Sub Total <span id="cart-subtotal">${{ number_format($subTotal) }}</span></li>
+							<li>Eco Tax <span id="cart-tax">${{ number_format($tax) }}</span></li>
+							<li>Shipping Cost <span>Free</span></li>
+							<li>Total <span id="cart-total">${{ number_format($final_subTotal) }}</span></li>
+						</ul>
+							<a class="btn btn-default update" href="">Update</a>
+							<a class="btn btn-default check_out" href="">Check Out</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section><!--/#do_action-->
+@endsection
+@section('scripts')
+<script>
+    $(document).ready(function(){
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+
+		$('.btn-update-qty').click(function(e) {
+			e.preventDefault();
+			let id = $(this).data('id');
+			let type = $(this).data('type');
+
+			$.ajax({
+				url: '{{ route("cart.update") }}',
+				method: "POST",
+				data: { id: id, type: type },
+				success: function(response) {
+					
+					if(response.status === true){
+						if(response.qty == 0) {
+							$('.row-item-' + id).fadeOut(300, function() { 
+								$(this).remove(); 
+							});
+						} else {
+							$('.qty-input-' + id).val(response.qty);
+							$('.item-total-' + id).text('$' + response.lineTotal);
+						}
+						$('#cart-quantity').text(response.total_quantity);
+						$('#cart-subtotal').text('$' + response.subTotal);
+						$('#cart-tax').text('$' + response.tax);
+						$('#cart-total').text('$' + response.total);
+					}
+				},
+			
+			});
+		});
+	
+		$('.btn-remove-item').click(function(e) {
+			e.preventDefault();
+			let id = $(this).data('id');
+
+			if(confirm("Bạn có chắc chắn muốn xóa sản phẩm này?")) {
+				$.ajax({
+					url: '{{ route("cart.remove") }}',
+					method: "POST",
+					data: { id: id },
+					success: function(response) {
+						
+						if(response.status === true){
+							$('.row-item-' + id).fadeOut(300, function() {
+								$(this).remove();
+							});
+							$('#cart-quantity').text(response.total_quantity);
+							$('#cart-subtotal').text('$' + response.subTotal);
+							$('#cart-tax').text('$' + response.tax);
+							$('#cart-total').text('$' + response.total);
+						}
+					},
+				});
+			}
+		});
+    });
+</script>
+@endsection
